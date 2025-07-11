ab7ecb8734084b613a82fd88fa9bd4fc3cd7f195
Logan Ferris <FLogan800@gmail.com>
2025-07-11 14:45:24 -0500

    Implement selection sort

---
+++ src/selection_sort.rs
@@ -0,0 +1,15 @@
+pub fn sort<T: PartialOrd>(data: &mut [T]) {
+    let len = data.len();
+
+    for i in 0..len {
+        let mut min_index = i;
+
+        for j in i + 1..len {
+            if data[j] < data[min_index] {
+                min_index = j;
+            }
+        }
+
+        data.swap(i, min_index);
+    }
+}
