3aa0cf6ef0611239d7af0741b5aec82a1753731a
Logan Ferris <FLogan800@gmail.com>
2025-07-15 23:04:40 -0500

    Update gnome sort

--- src/gnome_sort.rs
+++ src/gnome_sort.rs
@@ -4,7 +4,11 @@ pub fn sort<T: PartialOrd>(data: &mut [T]) {
     let mut i = 0;
 
     while i < len {
-        if i == 0 || data[i] >= data[i - 1] {
+        if i == 0 {
+            i += 1;
+        }
+
+        if data[i] >= data[i - 1] {
             i += 1;
         } else {
             data.swap(i, i - 1);
