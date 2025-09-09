23836fcf044a3ab37fcc3504ca9906ff72a2e93f
Logan Ferris <FLogan800@gmail.com>
2025-09-09 14:27:13 -0500

    Update merge in merge sort

--- src/merge_sort.rs
+++ src/merge_sort.rs
@@ -28,26 +28,33 @@ pub fn sort<T: Ord + Clone>(data: &mut [T]) {
 
 fn merge<T: Ord + Clone>(data: &mut [T], mid: usize) {
     let left = data[..mid].to_vec();
-    let right = data[mid..].to_vec();
+    let right = &data[mid..].to_vec();
 
     let mut i = 0;
     let mut j = 0;
+    let mut k = 0;
 
-    for element in data {
-        if i < left.len() && j < right.len() {
-            if left[i] < right[j] {
-                *element = left[i].clone();
-                i += 1;
-            } else {
-                *element = right[j].clone();
-                j += 1;
-            }
-        } else if i < left.len() {
-            *element = left[i].clone();
-            i += 1;
-        } else if j < right.len() {
-            *element = right[j].clone();
-            j += 1;
+    while i < left.len() && j < right.len() {
+        if left[i] < right[j] {
+            data[k] = left[i].clone();
+            i += 1
+        } else {
+            data[k] = right[j].clone();
+            j += 1
         }
+
+        k += 1;
+    }
+
+    while i < left.len() {
+        data[k] = left[i].clone();
+        i += 1;
+        k += 1;
+    }
+
+    while j < right.len() {
+        data[k] = right[j].clone();
+        j += 1;
+        k += 1;
     }
 }
