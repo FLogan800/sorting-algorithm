c5c1a3e7652420030a7935a91f0716827aba6739
Logan Ferris <FLogan800@gmail.com>
2025-09-18 10:51:19 -0500

    Implement basic tim sort logic

--- src/tim_sort.rs
+++ src/tim_sort.rs
@@ -1,3 +1,9 @@
+use crate::insertion_sort;
+use std::{cmp::min, f32::consts::LN_10};
+
+const MIN_MERGE: usize = 32;
+const MIN_GALLOP: i8 = 7;
+
 /// Sorts a data set using Tim Sort
 ///
 /// Average time complexity: O(n logn)
@@ -15,26 +21,133 @@
 ///     assert_eq!(data, [1, 2, 3, 4, 5]);
 /// }
 /// ```
-use crate::insertion_sort;
+pub fn sort<T: Ord + Clone>(data: &mut [T]) {
+    let n = data.len();
 
-const MIN_MERGE: usize = 32;
-const MIN_GALLOP: i8 = 7;
+    if n < 2 {
+        return;
+    }
 
-pub fn sort<T: Ord + Clone>(data: &mut [T]) {}
+    if n < MIN_MERGE {
+        insertion_sort::sort(data);
+        return;
+    }
 
-fn count_run_and_make_ascending<T: Ord + Clone>(data: &mut [T]) {}
+    let mut run_stack: Vec<(usize, usize)> = Vec::new(); // (start, len)
 
-fn min_run_length(n: usize) {}
+    let min_run = min_run_length(n);
+    let mut low = 0;
 
-fn merge_collapse() {}
+    while low < n {
+        let mut run_len = count_run_and_make_ascending(&mut data[low..]);
+
+        if run_len < min_run {
+            let target_len = min(low + min_run, n);
+            insertion_sort::sort(&mut data[low..target_len]);
+            run_len = target_len;
+        }
+
+        run_stack.push((low, run_len));
+        merge_collapse(data, &mut run_stack);
+
+        low += run_len
+    }
+
+    merge_force_collapse(&mut data, &mut run_stack);
+}
+
+fn count_run_and_make_ascending<T: Ord + Clone>(data: &mut [T]) -> usize {
+    let mut run_hi = 2;
+
+    if run_hi == data.len() {
+        return 1;
+    }
+
+    if data[1] < data[0] {
+        // Descending
+        while run_hi < data.len() && data[run_hi] < data[run_hi - 1] {
+            run_hi += 1;
+        }
+
+        data[..run_hi].reverse();
+    } else {
+        // Ascending
+        while run_hi < data.len() && data[run_hi] >= data[run_hi - 1] {
+            run_hi += 1;
+        }
+    }
+
+    return run_hi;
+}
+
+fn min_run_length(n: usize) -> usize {
+    let mut cur_n = n;
+    let mut r = 0;
+    while cur_n >= MIN_MERGE {
+        r |= cur_n & 1; // Checks if n is odd
+        cur_n >>= 1; // Divides cur_n by 2
+    }
+
+    cur_n + r
+}
+
+fn merge_collapse<T: Ord + Clone>(data: &mut [T], run_stack: &mut Vec<(usize, usize)>) {
+    let mut len = run_stack.len();
+
+    while len > 1 {
+        let mut n = len - 2;
+
+        if n > 0 && run_stack[n - 1].1 <= run_stack[n].1 + run_stack[n + 1].1
+            || n > 1 && run_stack[n - 2].1 <= run_stack[n].1 + run_stack[n - 1]
+        {
+            if run_stack[n - 1].1 < run_stack[n + 1].1 {
+                n -= 1;
+            } else if n < 0 || run_stack[n].1 > run_stack[n + 1].1 {
+                break;
+            }
+        }
+
+        merge_at(data, run_stack, n);
+    }
+}
 
 fn merge_force_collapse() {}
 
-fn merge_at() {}
+fn merge_at<T: Ord + Clone>(data: &mut [T], run_stack: &mut Vec<(usize, usize)>, i: usize) {
+    let (mut base1, mut len1) = run_stack[i];
+    let (mut base2, mut len2) = run_stack[i+1];
 
-fn gallop_left() {}
+    run_stack[i].1 = len1 + len2;
+    if run_stack.len() - i == 3 {
+        run_stack[i+1] = run_stack[i+2];
+    }
+    run_stack.pop();
 
-fn gallop_right() {}
+    let k = gallop_right();
+
+    base1 += k;
+    len1 -= k;
+
+    if len1 == 0 {
+        return;
+    }
+
+    len2 = gallop_left();
+
+    if len2 == 0 {
+        return;
+    }
+
+    if len1 <= len2 {
+        merge_low();
+    } else {
+        merge_high();
+    }
+}
+
+fn gallop_left<T: Ord>(key: T, data: &mut [T], hint: usize) -> usize {}
+
+fn gallop_right() -> usize {}
 
 fn merge_low() {}
 
